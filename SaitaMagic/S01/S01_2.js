7eff92ebea937296205cef1a481799b1592b036d
hirosphere <umoof.x@gmail.com>
2016-06-23 22:06:52 +0900

    16.06.23 -c

--- SaitaMagic/S01/S01_2.js
+++ SaitaMagic/S01/S01_2.js
@@ -1,38 +1,71 @@
 ﻿new function()
 {
-	var Context = AudioContext || webkitAudioContext;
-	
-	var context = new Context();
-	
-	context.AbsDT = function()
+	function au_new( delay, min_db )
 	{
-		return this.currentTime;
-	}
-	
-	var dc_src;
-	
-	new function()
-	{
-		var osc = context.createOscillator();
-		osc.type = "square";
-		osc.frequency.value = 0.1;
-		osc.frequency.setValueAtTime( 0, context.AbsDT() + 1 );
-		osc.start();
+		var Context = AudioContext || webkitAudioContext;
 		
-		var gain = context.createGain();
-		gain.gain.value = 7 / 6;
-		osc.connect( gain );
-		dc_src = gain;
+		if( Context )
+		{
+			var context = new Context();
+			
+			context.bt = function()
+			{
+				return this.currentTime + delay;
+			}
+			
+			var osc_0_hz = context.createOscillator();
+			osc_0_hz.type = "square";
+			osc_0_hz.frequency.value = 0.1;
+			osc_0_hz.frequency.setValueAtTime( 0, context.bt() + 1 );
+			osc_0_hz.start();
+			
+			var dc_src = context.createGain();
+			dc_src.gain.value = 7 / 6;
+			osc_0_hz.connect( dc_src );
+			
+			
+			context.CreateDCGain = function()
+			{
+				var gain = context.createGain();
+				dc_src.connect( gain );
+				return gain;
+			}
+			
+			
+			var exp_min = Math.pow( 10, min_db / 20 );
+			var exp_ramp_rate = min_db / -60;
+			
+			AudioParam.prototype.attack = function( time )
+			{
+				var t = context.bt();
+				
+				this.cancelScheduledValues( t );
+				this.setValueAtTime( 0, t );
+				this.linearRampToValueAtTime( 1, t + time );
+			}
+			
+			AudioParam.prototype.release = function( time )
+			{
+				var t = context.bt();
+				
+				var curv = this.value;
+				this.cancelScheduledValues( t );
+				//this.setValueAtTime( curv, t );
+				
+				var tend = t + time * exp_ramp_rate;
+				this.exponentialRampToValueAtTime( exp_min, tend );
+			}
+			
+			slog( "au_new()" );
+			
+			return context;
+		}
+		
+		return null;
 	}
 	
-	function CreateDCGain( init )
-	{
-		var gain = context.createGain();
-		dc_src.connect( gain );
-		return gain;
-	}
 	
-	var exp_min = 0.000001;
+	var context = au_new( 0, -60 );
 	
 	
 	//    //
@@ -88,6 +121,7 @@
 		
 		this.SetChord = function( ba, c1, c2, c3 )
 		{
+			slog( [ ba, c1, c2, c3 ] );
 			base_vo.NoteOn( ba );
 			ch_vo_1.NoteOn( c1 );
 			ch_vo_2.NoteOn( c2 );
@@ -120,7 +154,7 @@
 		var mod  = context.createOscillator();
 		var mod_amp =  context.createGain();
 		
-		var env = CreateDCGain();
+		var env = context.CreateDCGain();
 		
 		var amp =  context.createGain();
 		var amp_in1 =  context.createGain();
@@ -140,11 +174,11 @@
 		vosc.frequency.value = 440;
 		env.gain.value = 0;
 		amp.gain.value = 0;
-		amp.gain.setTargetAtTime( 0, context.AbsDT(), 0.1 );
+		amp.gain.setTargetAtTime( 0, context.bt(), 0.1 );
 		
 		mod.frequency.value = 1;
 		mod.detune.value = mod_pitch * 100;
-		mod_amp.gain.value = 0.8;
+		mod_amp.gain.value = 0.0;
 		
 		vosc.start();
 		mod.start();
@@ -153,20 +187,12 @@
 		
 		this.NoteOn = function( key )
 		{
-			vosc.detune.setValueAtTime( key * 100, context.AbsDT() );
+			vosc.detune.setValueAtTime( key * 100, context.bt() );
 			
 			if( ! note_on )
 			{
 				note_on = true;
-				
-				var attack = 0.3;
-				var t = context.AbsDT();
-				
-				env.gain.cancelScheduledValues( t );
-				//env.gain.setValueAtTime( env.gain.value, t );
-				env.gain.setValueAtTime( 0, t );
-				//env.gain.setTargetAtTime( 1, context.AbsDT(), attack );
-				env.gain.linearRampToValueAtTime( 1, t + attack );
+				env.gain.attack( 0.4 );
 			}
 		}
 		
@@ -175,18 +201,11 @@
 			if( note_on )
 			{
 				note_on = false;
-				
-				var release = 1.0;
-				var t = context.AbsDT();
-				
-				var curv = env.gain.value;
-				env.gain.cancelScheduledValues( t );
-				env.gain.setValueAtTime( curv, t + 0.01 );
-				//env.gain.setTargetAtTime( 0.4, t + 0.01, 1 );
-				//env.gain.setTargetAtTime( 0.2, t + 1, 1 );
-				env.gain.linearRampToValueAtTime( 0, t + release );
+				env.gain.release( 1.0 );
 			}
 		}
+		
+		slog( "\tVoice " + [ vosc, amp ] );
 	}
 	
 	
